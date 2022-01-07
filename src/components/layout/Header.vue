3d6d105ef6558aef7ceb65bcdf09fb3bfef6ecb4
신혁중 <hhhu04@gmail.com>
2022-01-07 18:57:26 +0900

    중간저장

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -10,7 +10,7 @@
       </b-nav-item-dropdown>
 
       <b-nav-item-dropdown text="User" right>
-        <b-dropdown-item href="/logout" v-if="token">Sign Out</b-dropdown-item>
+        <b-dropdown-item href="javascript:;" @click="logout" v-if="token">Sign Out</b-dropdown-item>
         <b-dropdown-item href="/mypage" v-if="token">My Page</b-dropdown-item>
         <b-dropdown-item href="/login" v-else>Sign In</b-dropdown-item>
       </b-nav-item-dropdown>
@@ -24,11 +24,14 @@ export default {
     name:"Header",
     data(){
         return  { 
-            token: this.$cookies.isKey("token")
+            token: this.$cookies.remove("token")
+          
         };
     },
     methods:{
-      
+      logout(){
+        this.$cookies.set
+      }
     }
 }
 </script>
--- src/views/feed/NewFeed.vue
+++ src/views/feed/NewFeed.vue
@@ -1,9 +1,103 @@
+<template>
+	<div>
+		<h1 class="title">게시판 등록</h1>
 
+		<div class="AddWrap">
+			<form>
+				<table class="tbAdd">
+					<colgroup>
+						<col width="15%" />
+						<col width="*" />
+					</colgroup>
+					<tr>
+						<th>제목</th>
+						<td><input type="text" v-model="subject" ref="subject" /></td>
+					</tr>
+					<tr>
+						<th>내용</th>
+						<td><textarea v-model="cont" ref="cont"></textarea></td>
+					</tr>
+          <tr>
+            <td>사진</td>
+            <!-- <td><input type="file" @change="onInutImage" multiple accept="image/*" id="file" ref="image"></td> -->
+            <td><input type="file" v-on:change="onInutImage" multiple accept="image/*" id="file" ref="image"></td>
+          </tr>
+				</table>
+			</form>
+		</div>
 
-// ,
-//   methods:{
-//     newContent(){
-//       console.log(this.content)
-//     }
-//   }
-  // <b-button block variant="outline-secondary" @click="newContent()">등록</b-button>
+		<div class="btnWrap">
+			<a href="javascript:;" @click="fnList" class="btn">목록</a>
+			<a href="javascript:;" @click="fnAddProc" class="btnAdd btn">등록</a>
+		</div>	
+	</div>
+</template>
+
+<script>
+export default {
+	data() { 
+		return{
+			subject:'',
+			cont:'',
+      img: ''
+		}
+	}
+	,methods:{
+    onInutImage(e){
+      var file = e.target.files[0]
+      this.img = window.URL.createObjectURL(file)
+      console.log(this.img)
+    },
+		fnList(){ 
+			// this.$router.push('/');
+			
+		}
+		,fnAddProc() { 
+			if(!this.subject) { 
+				alert("제목을 입력해 주세요");
+				this.$refs.subject.focus(); 
+				return;
+			}
+
+      const str = {
+                title : this.subject,
+                body:this.cont,
+                img:this.img
+            }
+
+			
+			this.$axios.post('http://192.168.88.128:8080/feed/new',str,{
+                headers: {
+                Authorization:this.$cookies.get("token"),
+            }
+        })
+			.then((res)=>{
+				if(res.data === 1) {
+					alert('등록되었습니다.');
+					this.fnList();
+				} else {
+					alert("실행중 실패했습니다.\n다시 이용해 주세요");
+				}
+			})
+			.catch((err)=>{
+				console.log(err);
+			})
+			
+		}
+	}	
+}
+</script>
+
+<style scoped>
+  h1 {text-align: center;}
+	.tbAdd{border-top:1px solid #888; width: 90%; text-align: center;}
+	.tbAdd th, .tbAdd td{border-bottom:1px solid #eee; padding:5px 0;}
+	.tbAdd td{padding:10px 10px; box-sizing:border-box;}
+	.tbAdd td input{width:100%; min-height:30px; box-sizing:border-box; padding:0 10px;}
+	.tbAdd td textarea{width:100%; min-height:300px; padding:10px; box-sizing:border-box;}
+	.btnWrap{text-align:center; margin:20px 0 0 0;}
+	.btnWrap a{margin:0 10px;}
+	.btnAdd {background:#43b984}
+	.btnDelete{background:#f00;}
+</style>
+
